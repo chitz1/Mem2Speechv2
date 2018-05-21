5527c3bcd0363db67596c404fc6f77eb29db23b6
jerome <jvgonzalvo@up.edu.ph>
2018-05-21 09:27:11 +0800

    Fixed SettingsActivity and SettingsFragment

--- app/src/main/java/gonzalvo/dpsm/cas/upm/edu/ph/mem2speech/SettingsActivity.java
+++ app/src/main/java/gonzalvo/dpsm/cas/upm/edu/ph/mem2speech/SettingsActivity.java
@@ -10,8 +10,9 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+        getFragmentManager().beginTransaction().replace(
+                android.R.id.content,
+                new SettingsFragment())
                 .commit();
     }
 }
--- app/src/main/java/gonzalvo/dpsm/cas/upm/edu/ph/mem2speech/SettingsFragment.java
+++ app/src/main/java/gonzalvo/dpsm/cas/upm/edu/ph/mem2speech/SettingsFragment.java
@@ -2,18 +2,14 @@ package gonzalvo.dpsm.cas.upm.edu.ph.mem2speech;
 
 
 import android.os.Bundle;
-import android.support.v7.preference.PreferenceFragmentCompat;
+import android.preference.PreferenceFragment;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
     }
-
-    @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-        setPreferencesFromResource(R.xml.preferences, rootKey);
-    }
-
+    
 }
