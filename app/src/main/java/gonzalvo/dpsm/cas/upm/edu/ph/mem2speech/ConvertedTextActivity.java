3fcf14d746d54991ae23e6ec17f91cff28bd3217
jerome <jvgonzalvo@up.edu.ph>
2018-05-22 10:59:40 +0800

    Moved toSpeech code to ConvertedTextActivity

--- app/src/main/java/gonzalvo/dpsm/cas/upm/edu/ph/mem2speech/ConvertedTextActivity.java
+++ app/src/main/java/gonzalvo/dpsm/cas/upm/edu/ph/mem2speech/ConvertedTextActivity.java
@@ -2,13 +2,21 @@ package gonzalvo.dpsm.cas.upm.edu.ph.mem2speech;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class ConvertedTextActivity extends AppCompatActivity {
 
+    private final int MAX_CHARS_TTS_CAN_READ = 3900;
+
     private TextView textView;
+    private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +28,48 @@ public class ConvertedTextActivity extends AppCompatActivity {
 
         textView = findViewById(R.id.converted_text);
         textView.setText(recognizedText);
+
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR){
+                    tts.setLanguage(Locale.US);
+                }
+            }
+        });
     }
 
     public void toSpeech(View view){
-        new ToSpeech(ConvertedTextActivity.this).execute(textView.getText().toString());
+        final String uId = this.hashCode() + "";
+        String text = textView.getText().toString();
+        if(text.length() >= MAX_CHARS_TTS_CAN_READ) {
+            final List<String> splittedText = splitText(text);
+            for(final String queueElement : splittedText) {
+                tts.speak(queueElement, TextToSpeech.QUEUE_ADD, null, uId);
+            }
+        } else {
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, uId);
+        }
+    }
+
+    private List<String> splitText(String text) {
+        final int textLength = text.length();
+        final List<String> splittedText = new ArrayList<>();
+
+        int index = 0;
+        while (index < textLength) {
+            splittedText.add(text.substring(index, Math.min(index + MAX_CHARS_TTS_CAN_READ, textLength)));
+            index += MAX_CHARS_TTS_CAN_READ;
+        }
+        return splittedText;
+    }
+
+    @Override
+    protected void onPause() {
+        if(tts != null){
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onPause();
     }
 }
